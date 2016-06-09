0d3ca2271663300bd0eb7f3b1b1eb08049156b3f
邱超凡 <fwage73@gmail.com>
2016-06-09 23:06:24 +0800

    temporarily comment init code

--- Classes/Controllers/GameController.cpp
+++ Classes/Controllers/GameController.cpp
@@ -17,6 +17,7 @@ bool GameController::init(void)
 //    auto roundSwitchListener = cocos2d::EventListenerCustom::create("RoundSwitch", [=](cocos2d::EventCustom* event){
 //        // TODO
 //    });
+#if 0
     auto localPlayer = LocalPlayer::create();
     auto remotePlayer = RemotePlayer::create();
     //auto AIplayer = AIPlayer::create();
@@ -32,35 +33,48 @@ bool GameController::init(void)
 //    });
     // _AIplayer = AIplayer;
     _localPlayer = localPlayer;
+    //_net = netWorkController;
+    _waitingDone = false;
+    _timeLeft = timeLeftDefault;
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(roundSwitchListener, this);
+    
+    this->schedule(schedule_selector(GameController::_handleBallStatus), ballStatusInterval);
+    //this->setCurrentPlayer(LOCAL_PLAYER); // todo 听netWork安排
     _remotePlayer = remotePlayer;
     // _net = netWorkController;
     // _waitingDone = false;
-    _timeLeft = timeLeftDefault;
-    _status = LOADING;
-    // _eventDispatcher->addEventListenerWithSceneGraphPriority(roundSwitchListener, this);
-    
-    this->schedule(schedule_selector(GameController::_handleBallStatus), ballStatusInterval);
-    this->addChild(localPlayer, 10);
-    this->addChild(remotePlayer, 10);
     // this->addChild(AIplayer, 10);
     this->addChild(contact, 0);
-    // this->addChild(netWorkController, 0);
-//    _eventDispatcher->addEventListenerWithFixedPriority(overRoundListener, 1);
-//    _eventDispatcher->addEventListenerWithFixedPriority(moveBallListener, 1);
+    _status = LOADING;
+    // _eventDispatcher->addEventListenerWithSceneGraphPriority(roundSwitchListener, this);
+#endif
+    
     return true;
 }
 
 // 当时钟到0，就跳入下一回合
 void GameController::_handleBallStatus(float dt)
 {
+#if 0
+    if(_currentPlayer == LOCAL_PLAYER) {
+    // this->addChild(netWorkController, 0);
+//    _eventDispatcher->addEventListenerWithFixedPriority(overRoundListener, 1);
+//    _eventDispatcher->addEventListenerWithFixedPriority(moveBallListener, 1);
+        //auto player = _AIplayer;
+    }
+    auto childs = _localPlayer->getChildren();
+    auto l = childs.begin();
+    while(l != childs.end()) {
+        auto node = *l;
+        int tag = node->getTag();
     if(_status == WAITING) {
         _timeLeft -= 1;
-    }
+                _timeLeft = timeLeftDefault;
     
 // remote player 时钟到0的时候不算回合结束，可能是网络延迟的问题，只有等到接受到overRound的数据才能算回合结束
     if(_timeLeft == 0 && _currentPlayer == LOCAL_PLAYER) {
-        this->_overRound();
-    }
+        //this->_overRound();
+            _timeLeft = timeLeftDefault;
 //    auto childs = _localPlayer->getChildren();
 //    auto l = childs.begin();
 //    while(l != childs.end()) {
@@ -88,16 +102,6 @@ void GameController::_handleBallStatus(float dt)
 //            this->_overRound();
 //        }
 //    }
-}
-
-
-// 进入下一回合
-void GameController::_overRound()
-{
-    _timeLeft = timeLeftDefault; // 时钟重置
-    if(_currentPlayer == LOCAL_PLAYER) {
-        _localPlayer->setActive(true);
-        _remotePlayer->setActive(false);
         _currentPlayer = REMOTE_PLAYER;
         _status = LOADING; // 状态: 等待数据
         this->sendData(); // todo 发送回合结束的信息
@@ -107,6 +111,7 @@ void GameController::_overRound()
         _currentPlayer = LOCAL_PLAYER;
         _status = WAITING; // 状态: 等待出招
     }
+#endif
 }
 
 // todo game controller给network发送数据
@@ -121,7 +126,7 @@ bool GameController::receiveData()
     return true; // 成功: true
 }
 
-void GameController::initNetwork(NetworkController*)
+void GameController::initNetwork(NetworkController* n)
 {
     printf("network init!");
     _status = READY;
